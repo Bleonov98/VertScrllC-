cd4b5505e42363f1a72dd14a438c8fa81bf5c8cf
Borys <Borys@DESKTOP-844MU72>
2022-08-05 00:44:45 +0300

    Add project files.

---
+++ VertScrllC++/Game.cpp
@@ -0,0 +1,276 @@
+#include "Game.h"
+
+void Game::SetPos(int x, int y)
+{
+	sprintf_s(coord, "%s%d;%dH", CSI, y, x);
+	printf(coord);
+}
+
+void Game::HotKeys(bool& pause)
+{
+	while (worldIsRun) {
+		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+			worldIsRun = false;
+		}
+		else if (GetAsyncKeyState(0x50) & 0x8000) {
+			pause = !pause;
+
+			this_thread::sleep_for(milliseconds(1000));
+		}
+	}
+}
+
+void Game::DrawArea()
+{
+	// Set console code page to UTF-8 so console known how to interpret string data
+	SetConsoleOutputCP(CP_UTF8);
+
+	// Enable buffering to prevent VS from chopping up UTF-8 byte sequences
+	setvbuf(stdout, nullptr, _IOFBF, 1000);
+
+	HRSRC hResource = FindResource(hInstance, MAKEINTRESOURCE(IDR_TEXT1), L"TEXT");
+
+	if (hResource)
+	{
+		HGLOBAL hLoadedResource = LoadResource(hInstance, hResource);
+
+		if (hLoadedResource)
+		{
+			LPCSTR area = (LPCSTR)LockResource(hLoadedResource);
+
+			if (area)
+			{
+				DWORD dwResourceSize = SizeofResource(hInstance, hResource);
+
+				if (0 != dwResourceSize)
+				{
+					int row = ROWS * 30, col = 0;
+					for (int i = COLS * (ROWS * 30); i > 0; i--) {
+						GameMap[row][col] = area[i];
+						if (i == 0 && i < COLS * (ROWS * 30)) {
+							row--;
+							col = 0;
+						}
+						col++;
+					}
+				}
+			}
+		}
+	}
+
+	for (int h = 0; h < ROWS; h++)
+	{
+		for (int w = 0; w < COLS; w++)
+		{
+			cout << GameMap[h][w];
+			activeAreaBuf[h][w] = GameMap[h][w];
+		}
+	}
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+}
+
+void Game::CreateWorld() {
+
+	term.Terminal();  // Set virtual terminal settings
+	term.SetScreenSize();
+	term.SetConsoleFont();
+
+	printf(CSI "?1049h"); // enable alt buffer
+	printf(CSI "?25l"); // hide cursor blinking
+
+	DrawArea();
+}
+
+void Game::DrawEndInfo(bool& restart)
+{
+	if (win) {
+		SetPos(44, ROWS + 2);
+		cout << "CONGRATULATION! YOU WIN!";
+	}
+	else {
+		SetPos(50, ROWS + 2);
+		cout << "GAME OVER!";
+	}
+
+	SetPos(45, ROWS + 3);
+	cout << "PRESS ENTER TO RESTART";
+	SetPos(47, ROWS + 4);
+	cout << "PRESS ESC TO EXIT";
+
+	bool pressed = false;
+	restart = false;
+
+	while (!pressed) {
+		if (GetAsyncKeyState(VK_RETURN)) {
+			restart = true;
+			pressed = true;
+		}
+		else if (GetAsyncKeyState(VK_ESCAPE)) {
+			restart = false;
+			pressed = true;
+		}
+	}
+}
+
+void Game::DrawChanges()
+{
+	for (int y = 0; y < ROWS; y++)
+	{
+		for (int x = 0; x < COLS; x++)
+		{
+			if (prevBuf[y][x] == wData.vBuf[y][x]) {
+				continue;
+			}
+			else {
+				prevBuf[y][x] = wData.vBuf[y][x];
+
+				SetPos(x, y);
+
+				if ((prevBuf[y][x] >> 8) == Red) {
+					printf(CSI "22;31m");
+				}
+				else if ((prevBuf[y][x] >> 8) == Green) {
+					printf(CSI "22;32m");
+				}
+				else if ((prevBuf[y][x] >> 8) == Blue) {
+					printf(CSI "22;34m");
+				}
+				else if ((prevBuf[y][x] >> 8) == Yellow) {
+					printf(CSI "22;33m");
+				}
+				else if ((prevBuf[y][x] >> 8) == Purple) {
+					printf(CSI "22;35m");
+				}
+				else if ((prevBuf[y][x] >> 8) == Cyan) {
+					printf(CSI "22;36m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrRed) {
+					printf(CSI "22;91m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrGreen) {
+					printf(CSI "22;92m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrBlue) {
+					printf(CSI "22;94m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrCyan) {
+					printf(CSI "22;96m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrPurple) {
+					printf(CSI "22;95m");
+				}
+				else if ((prevBuf[y][x] >> 8) == BrYellow) {
+					printf(CSI "22;93m");
+				}
+				else if ((prevBuf[y][x] >> 8) == White) {
+					printf(CSI "47;37m");
+				}
+				else printf(CSI "22; 44m");
+
+				cout << char(prevBuf[y][x]);
+
+				printf(CSI "1;0m");
+			}
+		}
+	}
+}
+
+void Game::DrawToMem()
+{
+	//for (int i = 0; i < enemyList.size(); i++)
+	//{
+	//	if (enemyList[i]->IsObjectDelete()) {
+	//		enemyList.erase(enemyList.begin() + i);
+	//		i = -1;
+	//	}
+	//}
+
+	//for (int i = 0; i < bulletList.size(); i++)
+	//{
+	//	if (bulletList[i]->IsObjectDelete()) {
+	//		bulletList.erase(bulletList.begin() + i);
+
+	//		i = -1;
+	//	}
+	//}
+
+	//for (int i = 0; i < bonusList.size(); i++)
+	//{
+	//	if (bonusList[i]->IsObjectDelete()) {
+	//		bonusList.erase(bonusList.begin() + i);
+
+	//		i = -1;
+	//	}
+	//}
+
+	/*for (int i = 0; i < allObjectList.size(); i++)
+	{
+		if (allObjectList[i]->IsObjectDelete()) {
+			wData.vBuf[allObjectList[i]->GetY()][allObjectList[i]->GetX()] = u' ';
+			allObjectList.erase(allObjectList.begin() + i);
+
+			i = -1;
+		}
+	}
+
+	for (int i = 0; i < allObjectList.size(); i++)
+	{
+		allObjectList[i]->DrawObject();
+	}*/
+}
+
+void Game::RunWorld(bool& restart)
+{
+	srand(time(NULL));
+	CreateWorld();
+
+	bool pause = false;
+	score = 0;
+
+	thread hotKeys([&]
+		{ HotKeys(pause); }
+	);
+
+	int tick = 0;
+	int scrollSpeed = 2;
+
+	while (worldIsRun) {
+
+		if (pause) {
+
+			SetPos(COLS / 2 - 2, ROWS / 2);
+			cout << "PAUSED";
+
+			while (pause) {
+
+			}
+
+			SetPos(COLS / 2 - 2, ROWS / 2);
+			cout << "      ";
+
+		}
+
+		//Collision(player);
+
+		//WallCollision(player);
+
+		DrawToMem();
+
+		DrawChanges();
+
+		if (tick % scrollSpeed == 0 && tick > 0) ScrollWindow();
+
+		//DrawInfo(player);
+
+		Sleep(10);
+
+		tick++;
+	}
+
+	DrawEndInfo(restart);
+
+	hotKeys.join();
+
+	printf(CSI "?1049l");
+}
---
+++ VertScrllC++/Game.h
@@ -0,0 +1,225 @@
+#include "GameObject.h"
+
+
+class Game
+{
+public:
+    wd wData;
+
+    char16_t prevBuf[ROWS][COLS];
+    char coord[100];
+
+    char GameMap[ROWS*30][COLS];
+
+    char prevActiveAreaBuf[ROWS][COLS];
+    char activeAreaBuf[ROWS][COLS];
+
+    int scrollY = 0;
+
+    bool worldIsRun = true, win = false, alreadySpawn = false;
+
+    int score = 0;
+
+    vector<GameObject*> allObjectList;
+
+    HINSTANCE hInstance;
+
+    class VirtualTerminal {
+    public:
+
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+        _SMALL_RECT Rect;
+
+        HWND hWindowConsole = GetForegroundWindow();
+
+        int Width = 90, Height = 56, err = 30;
+
+        bool Terminal() {
+
+            // Set output mode to handle virtual terminal sequences
+            if (hOut == INVALID_HANDLE_VALUE)
+            {
+                return false;
+            }
+            HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+            if (hIn == INVALID_HANDLE_VALUE)
+            {
+                return false;
+            }
+
+            DWORD dwOriginalOutMode = 0;
+            DWORD dwOriginalInMode = 0;
+            if (!GetConsoleMode(hOut, &dwOriginalOutMode))
+            {
+                return false;
+            }
+            if (!GetConsoleMode(hIn, &dwOriginalInMode))
+            {
+                return false;
+            }
+
+            DWORD dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+            DWORD dwRequestedInModes = ENABLE_VIRTUAL_TERMINAL_INPUT;
+
+            DWORD dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
+            if (!SetConsoleMode(hOut, dwOutMode))
+            {
+                // we failed to set both modes, try to step down mode gracefully.
+                dwRequestedOutModes = ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+                dwOutMode = dwOriginalOutMode | dwRequestedOutModes;
+                if (!SetConsoleMode(hOut, dwOutMode))
+                {
+                    // Failed to set any VT mode, can't do anything here.
+                    return -1;
+                }
+            }
+
+            DWORD dwInMode = dwOriginalInMode | ENABLE_VIRTUAL_TERMINAL_INPUT;
+            if (!SetConsoleMode(hIn, dwInMode))
+            {
+                // Failed to set VT input mode, can't do anything here.
+                return -1;
+            }
+
+            return 0;
+        }
+
+        void SetScreenSize() {
+            COORD monitorSize = { GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN) };
+            COORD bufferSize = { Width, Height };
+
+            _SMALL_RECT Rect;
+            Rect.Top = 0;
+            Rect.Left = 0;
+            Rect.Bottom = Height - 1;
+            Rect.Right = Width - 1;
+
+            SetConsoleScreenBufferSize(hOut, bufferSize);
+
+            SetWindowPos(hWindowConsole, HWND_NOTOPMOST,
+                (monitorSize.X / 2 - Width * 4 - err * 4), (monitorSize.Y / 2 - Height * 8 - err), 0, 0,
+                SWP_NOZORDER | SWP_NOREPOSITION | SWP_NOREDRAW);
+
+            SetConsoleWindowInfo(hOut, TRUE, &Rect);
+
+            SetWindowLong(hWindowConsole, GWL_STYLE, GetWindowLong(hWindowConsole, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
+
+            SetConsoleTitleA("ScrollerTypeGame C++");
+        }
+
+        void SetConsoleFont() {
+
+            CONSOLE_FONT_INFOEX cfi;
+
+            cfi.cbSize = sizeof(CONSOLE_FONT_INFOEX);
+            cfi.nFont = 6;
+            cfi.dwFontSize.X = 30;
+            cfi.dwFontSize.Y = 13;
+            cfi.FontFamily = 54;
+            cfi.FontWeight = 1000;
+
+            wcscpy_s(cfi.FaceName, L"Lucida Console");
+            SetCurrentConsoleFontEx(hOut, 0, &cfi);
+        }
+    };
+
+    void ScrollWindow() {
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                wData.grid[i][j] = 0;
+            }
+        }
+
+        for (int i = scrollY; i < scrollY + ROWS; i++)
+        {
+            int activeY = 0;
+            for (int j = 0; j < COLS; j++)
+            {
+                activeAreaBuf[activeY][j] = GameMap[i][j];
+
+                if (activeAreaBuf[activeY][j] == '=') {
+                    wData.grid[activeY][j] = -1;
+                }
+
+                activeY++;
+            }
+        }
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (i < 2 && i >= ROWS - 1) continue;
+                if ((prevActiveAreaBuf[i][j] != activeAreaBuf[i][j]) && (wData.vBuf[i][j] == 0 || wData.vBuf[i][j] == u' '))
+                {
+                    prevActiveAreaBuf[i][j] = activeAreaBuf[i][j];
+
+                    SetPos(j, i + 1);
+
+                    cout << prevActiveAreaBuf[i][j];
+                }
+            }
+        }
+
+ /*       if (scrollX + COLS < 1023) scrollX++;
+
+        if (scrollX % 120 == 0 && scrollX > 0) {
+            SpawnEnemy(COLS - 10, 3 + rand() % (ROWS - 6), REGULAR);
+        }
+        if (scrollX % 50 == 0 && scrollX > 0) {
+            SpawnEnemy(COLS - 10, 3 + rand() % (ROWS - 6), SMALL);
+        }
+        if (scrollX % 200 == 0 && scrollX > 0) {
+            SpawnEnemy(COLS - 10, ROWS - 3, LAND);
+        }
+        if (scrollX + COLS == 1020) {
+            SpawnEnemy(COLS - 10, 3 + rand() % (ROWS - 6), BOSS);
+            PlaySound(MAKEINTRESOURCE(IDR_WAVE3), NULL, SND_RESOURCE | SND_ASYNC);
+        }*/
+    }
+
+    VirtualTerminal term; // console setting
+
+protected:
+
+    void SetPos(int x, int y);
+
+    void DrawArea();
+
+    void CreateWorld();
+
+    void DrawToMem();
+
+    void HotKeys(bool& pause);
+
+    void DrawEndInfo(bool& restart);
+
+    //void DrawInfo(Player* player);
+
+    void DrawChanges();
+
+    //void SpawnEnemy(int x, int y, int type);
+
+    //void SpawnBonus(Enemy* enemy, int type);
+
+    //void ReloadGun(Player* player);
+
+    //void ReloadEnGun(Enemy* enemy);
+
+    //void Shot(int owner, GameObject* gmObj, Player* player);
+
+    //void Collision(Player* player);
+
+    //void WallCollision(Player* player);
+
+public:
+
+    void RunWorld(bool& restart);
+
+};
